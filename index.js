8615d5e75311cab840d6d24bf4e94a41d5aa3ff1
Isaiah <IaguileraCFE@gmail.com>
2023-11-10 12:08:12 -0800

    Last revised commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code in this file!
+const currentUser = "Isaiah"
+
+const welcomeMessage = `Welcome to Flatbook,   ${currentUser}!`
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+//console.log(excitedWelcomeMessage)
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`
+console.log(shortGreeting)
